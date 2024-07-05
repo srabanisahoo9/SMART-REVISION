a83bacddbe540658f6fb65352198f53ef7bc80d8
Mujtaba Hussain <mujtabah258@gmail.com>
2024-07-05 22:35:10 +0530

    route fixed

--- basicreq/src/page/home.js
+++ basicreq/src/page/home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClick = (path) => {
+    console.log(path);
+    navigate(path);
+  };
   return (
     <>
       <h1>this is home page</h1>
+      <button onClick={() => handleClick("/login")}>login</button>
+      <button onClick={() => handleClick("/register")}>register</button>
     </>
   );
 };
