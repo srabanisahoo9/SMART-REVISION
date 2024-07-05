8791001d8fbc0856d5e168fbb16436e09ac75461
srabanisahoo9 <srabanis822@gmail.com>
2024-07-06 01:00:00 +0530

    committed

---
+++ basicreq/src/App.js
@@ -0,0 +1,15 @@
+import Register from './register';
+import Login from './loginpage';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Login/>
+      <Register />
+
+    </main>
+  );
+}
+
+export default App;
