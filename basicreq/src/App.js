9789cd2e52f524c5604c3696b4590d0a9ed2d279
Mujtaba Hussain <mujtabah258@gmail.com>
2024-07-06 21:03:46 +0530

    minor changes

--- basicreq/src/App.js
+++ basicreq/src/App.js
@@ -2,16 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./page/auth/register";
 import Login from "./page/auth/login";
 import Home from "./page/home";
-
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+    </Routes>
   );
 };
 
--- basicreq/src/index.js
+++ basicreq/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/Authprovider";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 // Create root and render the application
 const root = createRoot(document.getElementById("root"));
@@ -10,7 +11,9 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
